af102f66e3f2e800c2f6f73edca373555c282e64
Adam Russell <adamlr6+pub@gmail.com>
2014-01-24 00:34:32 -0500

    Initial commit.
    Install LightDM, XFCE, Vim, Git, Chrome, Oracle JDK 7, and IntelliJ IDEA
    13.

---
+++ files/java.sh
@@ -0,0 +1,3 @@
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+export CLASSPATH=${JAVA_HOME}/lib/classes.zip
+export PATH=${JAVA_HOME}/bin:${PATH}
---
+++ manifests/init.pp
@@ -0,0 +1,38 @@
+class devenv ($user = 'vagrant') {
+  include java7
+  include apt
+  Class['apt'] -> Package <| |>
+
+  package { 'lightdm': }
+
+  file { '/etc/lightdm/lightdm.conf':
+    content => template('devenv/lightdm.conf.erb'),
+    require => Package['lightdm']
+  }
+
+  package { 'xfce4': }
+
+  package { 'xfce4-goodies':
+    require => Package['xfce4']
+  }
+
+  package { 'vim-gtk': }
+
+  package { 'git': }
+
+  class { 'googlechrome':
+    require => Package['xfce4']
+  }
+
+  file { '/etc/profile.d/java.sh':
+    source => 'puppet:///modules/devenv/java.sh',
+    require => Class['java7']
+  }
+
+  class { 'idea::community':
+    version => '13.0.2',
+    build => '133.696',
+    require => [Package['xfce4'],
+                Class['java7']]
+  }
+}
