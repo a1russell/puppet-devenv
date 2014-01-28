56922a876accef287a78e7e2b00f4cbbafaf48d5
Adam Russell <adamlr6+pub@gmail.com>
2014-01-28 16:11:42 -0500

    Set Chrome as default browser.

--- manifests/init.pp
+++ manifests/init.pp
@@ -26,6 +26,37 @@ class devenv ($user = 'vagrant') {
     require => Package['xfce4']
   }
 
+  exec { 'update-alternatives --set x-www-browser \
+          /usr/bin/google-chrome-stable':
+    path => '/usr/bin:/bin',
+    unless => 'update-alternatives --query x-www-browser |\
+               grep Value | grep -q google-chrome-stable',
+    require => Class['googlechrome']
+  }
+
+  exec { "echo 'x-scheme-handler/http=exo-web-browser.desktop' >>\
+          /home/${user}/.local/share/applications/mimeapps.list":
+    path => '/bin',
+    unless => "grep -q 'x-scheme-handler/http\s*=' \
+               /home/${user}/.local/share/applications/mimeapps.list",
+    require => Class['googlechrome']
+  }
+
+  exec { "echo 'x-scheme-handler/https=exo-web-browser.desktop' >>\
+          /home/${user}/.local/share/applications/mimeapps.list":
+    path => '/bin',
+    unless => "grep -q 'x-scheme-handler/https\s*=' \
+               /home/${user}/.local/share/applications/mimeapps.list",
+    require => Class['googlechrome']
+  }
+
+  file { "/home/${user}/.config/xfce4/helpers.rc":
+    owner => $user,
+    group => $user,
+    source => 'puppet:///modules/devenv/helpers.rc',
+    require => Class['googlechrome']
+  }
+
   file { '/etc/profile.d/java.sh':
     source => 'puppet:///modules/devenv/java.sh',
     require => Class['java7']
