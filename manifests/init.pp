7daf84818ce98c519f09d948e171d19b3f258acb
Adam Russell <adamlr6+pub@gmail.com>
2014-04-21 23:25:42 -0400

    Upgrade Gradle to 1.11.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 class devenv ($username = 'vagrant') {
   $idea_version = '13.1.2'
   $idea_build = '135.690'
-  $gradle_version = '1.10'
+  $gradle_version = '1.11'
   $scala_version = '2.10.3'
 
   include apt
